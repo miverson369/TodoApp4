c4c8aa8b49563416521adf4921b23bf3b0f1d4fb
miverson369 <mila.iverson@student.centralia.edu>
2025-03-23 12:56:59 -0700

    Add files via upload

--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,5 @@
+using System.Drawing.Text;
+
 namespace TodoApp4
 {
     public partial class Form1 : Form
@@ -5,6 +7,53 @@ namespace TodoApp4
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void BtnClear_Click(object sender, EventArgs e, string newTextBox)
+
+        {
+            DateTime date = DateTime.Parse(newTextBox);
+
+            //where we handle the add event
+            Todo mytodo = new Todo(newTextBox, new DateTime(), false); }
+        
+        //where we gather the user input from textbox
+        private void BtnAdd_Click(object sender, EventArgs e, string newTextBox, object listBoxDisplay)
+        {
+            string userInput = GetUserInput(newTextBox);
+            //add the text to the listbox
+            object value = listBoxDisplay.Items.Add(userInput);
+            //Clear the Textbox for new input
+          
+            
+
+
+            //MessageBox.Show(mytodo.ToString());
+
+            //check if the todo is valid
+            //if it is not, display an error
+            //exit
+
+        }
+
+        private static object GetUserInput(string newTextBox)
+        {
+            return newTextBox.Text;
+        }
+
+        private void BtnClear_Clear
+            (object sender, EventArgs e)
+        {
+            TaskDescription.Clear();
+            DueDate.Clear();
+            MessageBox.Show("Form Cleared");
+
+        }
+
+        private void BtnAdd_Add
+            (object sender, EventArgs e)
+        {
+
         }
     }
 }
---
+++ Todo.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoApp4
+{
+    internal class Todo
+        {
+            public static int ObjectCounter = 0;
+            public int Id { get; set; }
+            public string TaskDescription { get; set; }
+            public DateTime DueDate { get; set; }
+            public bool IsDone { get; set; }
+
+            public Todo(string description, DateTime date, bool doneState)
+        {
+            ObjectCounter++;
+            Id = ObjectCounter;
+            TaskDescription = description;
+            DueDate = date;
+            IsDone = doneState;
+        }
+
+        public Todo(ReadOnlySpan<char> newTextBox, DateTime dateTime, bool v)
+        {
+        }
+
+        public override string? ToString()
+        {
+            return $"{Id} - {DueDate} - {TaskDescription}";
+
+        }
+    }
+}
+
+   
